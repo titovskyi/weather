26a46c9ba31cd4d5dd8c13b06e8b94d4d1b137a8
titovskyi <titovskyi@gmail.com>
2018-06-03 12:59:07 +0300

    deploy v.3

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
